e6699cf9486e1f7d83f048beaf4de4ba37071d11
Mohamed Assem <mo7mad.assim@gmail.com>
2020-09-14 17:49:56 +0200

    Setup Room Database

---
+++ app/src/main/java/com/assem/runningtracker/app/BaseApp.kt
@@ -0,0 +1,15 @@
+package com.assem.runningtracker.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Mohamed Assem on 14-Sep-20.
+ * mohamed.assem.ali@gmail.com
+ */
+
+@HiltAndroidApp
+class BaseApp : Application() {
+
+}
---
+++ app/src/main/java/com/assem/runningtracker/di/AppModule.kt
@@ -0,0 +1,15 @@
+package com.assem.runningtracker.di
+
+import com.assem.runningtracker.app.BaseApp
+import dagger.Module
+import dagger.hilt.InstallIn
+
+
+/**
+ * Created by Mohamed Assem on 14-Sep-20.
+ * mohamed.assem.ali@gmail.com
+ */
+@Module
+@InstallIn(BaseApp::class)
+object AppModule {
+}
