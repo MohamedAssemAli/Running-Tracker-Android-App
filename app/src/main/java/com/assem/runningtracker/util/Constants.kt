e7c9020b6d31a2e711e89182609e9edbdc5019cb
Mohamed Assem <mo7mad.assim@gmail.com>
2020-09-15 16:26:44 +0200

    Location Service Background Setup

--- app/src/main/java/com/assem/runningtracker/util/Constants.kt
+++ app/src/main/java/com/assem/runningtracker/util/Constants.kt
@@ -7,7 +7,7 @@ package com.assem.runningtracker.util
  */
 
 object Constants {
-    const val  RUNNING_DATABASE_NAME = "running_db"
+    const val RUNNING_DATABASE_NAME = "running_db"
     const val REQUEST_CODE_LOCATION_PERMISSION = 0
     const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
@@ -16,4 +16,6 @@ object Constants {
     const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
     const val NOTIFICATION_ID = 1
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
 }
