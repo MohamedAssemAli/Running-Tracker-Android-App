775dc168307036378a24ba46d76bd8760ac8f045
Mohamed Assem <mo7mad.assim@gmail.com>
2020-10-04 13:38:07 +0200

    Sorting runs

--- app/src/main/java/com/assem/runningtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/assem/runningtracker/ui/fragments/RunFragment.kt
@@ -4,14 +4,17 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.assem.runningtracker.R
 import com.assem.runningtracker.ui.MainViewModel
 import com.assem.runningtracker.ui.adapters.RunsAdapter
 import com.assem.runningtracker.util.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.assem.runningtracker.util.SortType
 import com.assem.runningtracker.util.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
@@ -36,7 +39,34 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
         super.onViewCreated(view, savedInstanceState)
         requestPermissions()
         setupRecyclerView()
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, {
+        when (viewModel.sortType) {
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.DISTANCE -> spFilter.setSelection(2)
+            SortType.AVG_SPEED -> spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spFilter.setSelection(4)
+        }
+
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                pos: Int,
+                id: Long
+            ) {
+                when (pos) {
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runsAdapter.submitList(it)
         })
         fab.setOnClickListener {
